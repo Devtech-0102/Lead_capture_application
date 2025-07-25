66f0d99fd2b8fb4b8da68315ae0d2753bde8f340
Sathish <sathish9585482928@gmail.com>
2025-07-25 17:40:49 +0530

    layout structure

--- Lead_Capture_Application/src/App.jsx
+++ Lead_Capture_Application/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1>
-        Click on the Vite and React logos to learn more
-      </h1>
+      <Home />
     </>
   )
 }
